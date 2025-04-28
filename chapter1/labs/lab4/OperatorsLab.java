52d36e2eb08edeaaa131bb959da00b877f6e8b30
hoon <mhoo999@naver.com>
2025-04-28 16:23:09 +0900

    1일차

--- chapter1/labs/lab4/OperatorsLab.java
+++ chapter1/labs/lab4/OperatorsLab.java
@@ -7,29 +7,53 @@ package chapter1.labs.lab4;
  */
 public class OperatorsLab {
     public static void main(String[] args) {
+
+        int a = 5;
+        int b = 3;
+        int c = 6;
+        int d = 7;
+
         // TODO: 1. 산술 연산자 사용하기
         // 두 개의 정수형 변수를 선언하고 덧셈, 뺄셈, 곱셈, 나눗셈, 나머지 연산을 수행하세요.
-        
-        
+        System.out.println("a + b = " + (a + b));
+        System.out.println("a - b = " + (a - b));
+        System.out.println("a * b = " + (a * b));
+        System.out.println("a / b = " + (a / b));
+        System.out.println("a % b = " + (a % b));
+
+        System.out.print("\n");
         // TODO: 2. 증감 연산자 사용하기
         // 변수를 선언하고 전위 증가, 후위 증가, 전위 감소, 후위 감소 연산을 수행하세요.
         // 각 단계별로 변수의 값을 출력하여 변화를 확인하세요.
-        
-        
+        System.out.println("a++ = " + a++);
+        System.out.println("a = " + a);
+        System.out.println("++a = " + ++a);
+        System.out.println("a = " + a);
+
+        System.out.println("b-- = " + b--);
+        System.out.println("b = " + b);
+        System.out.println("--b = " + --b);
+        System.out.println("b = " + b);
+
+        System.out.print("\n");
         // TODO: 3. 비교 연산자 사용하기
         // 두 변수를 비교하여 결과를 출력하세요 (==, !=, >, <, >=, <=).
-        
-        
+        System.out.println("a == b = " + (a == b));
+        System.out.println("a == b = " + (a == c));
+        System.out.println("a == b = " + (a == b));
+        System.out.println("a == b = " + (a == b));
+
+        System.out.print("\n");
         // TODO: 4. 논리 연산자 사용하기
         // boolean 변수를 사용하여 AND(&&), OR(||), NOT(!) 연산을 수행하세요.
-        
-        
+
+        System.out.print("\n");
         // TODO: 5. 대입 연산자 사용하기
         // 변수를 선언하고 복합 대입 연산자(+=, -=, *=, /=, %=)를 사용하여 값을 변경하세요.
-        
-        
+
+        System.out.print("\n");
         // TODO: 6. 삼항 연산자 사용하기
         // 조건에 따라 다른 값을 할당하는 예제를 작성하세요.
-        
+
     }
 } 
